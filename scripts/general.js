7b77ffd2dd5fc2aab2e04cc793caceaf2aa419d5
Pestmite <Charleshenrichaki@gmail.com>
2025-11-07 12:44:35 -0500

    Added mobile header

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,9 @@
 const header = document.querySelector('header');
-const subheaderLink = document.querySelectorAll('a[href^="#"]')
+const subheaderLink = document.querySelectorAll('a[href^="#"]');
+const menu = document.querySelector('.menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
 
+/* Satisfying Scrolling */
 subheaderLink.forEach(navLink => {
   navLink.addEventListener('click', function (event) {
     document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -11,6 +14,7 @@ subheaderLink.forEach(navLink => {
   });
 });
 
+/* Header disappearing */
 let startY = 0;
 let HIDEMIN = 80;
 
@@ -28,4 +32,16 @@ window.addEventListener('scroll', () => {
   }
 
   startY = currentY;
+});
+
+/* Mobile Menu */
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  if (!offScreenMenu.contains(e.target) && !menu.contains(e.target)) {
+    offScreenMenu.classList.remove('active');
+  };
 });
