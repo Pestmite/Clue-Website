b11c503866d25a2ed9aece69447a8fa77cc8d202
Pestmite <Charleshenrichaki@gmail.com>
2025-11-08 10:16:15 -0500

    Added custom cursor

--- scripts/general.js
+++ scripts/general.js
@@ -2,6 +2,7 @@ const header = document.querySelector('header');
 const subheaderLink = document.querySelectorAll('a[href^="#"]');
 const menu = document.querySelector('.menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const cursorElement = document.getElementById('cursor');
 
 /* Satisfying Scrolling */
 subheaderLink.forEach(navLink => {
@@ -22,7 +23,7 @@ window.addEventListener('scroll', () => {
   const currentY = window.scrollY;
 
   if (currentY > HIDEMIN) {
-    if (currentY > startY && innerWidth > 660) {
+    if (currentY > startY && innerWidth > 800) {
       header.style.transform = 'translateY(-150%)';
     } else {
       header.style.transform = 'translateY(0)';
@@ -44,4 +45,18 @@ document.addEventListener('click', (e) => {
   if (!offScreenMenu.contains(e.target) && !menu.contains(e.target)) {
     offScreenMenu.classList.remove('active');
   };
+});
+
+document.addEventListener('mousemove', (e) => {
+  cursorElement.style.left = `${e.clientX}px`;
+  cursorElement.style.top = `${e.clientY}px`;
+
+  const el = document.elementFromPoint(e.clientX, e.clientY);
+  const cursor = window.getComputedStyle(el).cursor;
+
+  if (cursor === "pointer" && !cursorElement.classList.contains('.cursor-pointer')) {
+    cursorElement.classList.add('cursor-pointer');
+  } else {
+    cursorElement.classList.remove('cursor-pointer');
+  }
 });
